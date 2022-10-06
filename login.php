f12dd982313a2fac7566759f8de8a8bc08235eb0
John Moises Paunlagui <paunlagui.jm.cs@gmail.com>
2022-10-06 16:51:27 +0800

    .htaccess modifications

--- login.php
+++ login.php
@@ -8,12 +8,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Login - Brand</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Itim&amp;display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/vanilla-zoom.min.css">
+    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
 </head>
 
 <body class="bg-light">
@@ -24,7 +24,7 @@
                 <img alt="logo" src="<?php echo BASE_URL . "/assets/img/logo-light.png"; ?>" style="width: 150px;">
                 <h3 class="fs-2 fw-bold text-info pt-0 mt-3 mb-3" style="font-family: Itim, serif;"><sup><span style="color: rgba(var(--bs-info-rgb), var(--bs-text-opacity)) ;">Employee Attendance Monitoring System</span></sup><br></h3>
             </div>
-            <form class="fw-light mt-0 pt-4 pb-4" style="height: 400.4px;" method="post">
+            <form class="fw-light mt-0 pt-4 pb-4" style="height: 400px;" method="post">
                 <h2 class="fw-semibold text-info mb-4 mt-0">Sign In</h2>
                 <div class="mb-3">
                     <label for="username" class="form-label">Email</label>
@@ -39,10 +39,10 @@
         </div>
     </section>
 </main>
-<script src="<?php echo BASE_URL; ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
-<script src="<?php echo BASE_URL; ?>/assets/js/vanilla-zoom.js"></script>
-<script src="<?php echo BASE_URL; ?>/assets/js/theme.js"></script>
+<script src="assets/js/vanilla-zoom.js"></script>
+<script src="assets/js/theme.js"></script>
 </body>
 </html>
 
